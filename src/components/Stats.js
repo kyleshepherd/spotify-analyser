add89a1d2b950a31f602091e7f1fb4ba936982de
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-09-01 18:37:46 +0100

    Final stat screen styling

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,11 +6,16 @@ import { ReactComponent as Drum } from '../svg/drum.svg'
 const Stats = ({ stats }) => {
 	return (
 		<div className="mx-auto max-w-2xl">
-			<StatBar name="Acousticness" stat={stats.acousticness} />
-			<StatBar name="Danceability" stat={stats.danceability} />
-			<StatBar name="Energy" stat={stats.energy} />
-			<StatBar name="Instrumentalness" stat={stats.instrumentalness} />
-			<StatBar name="Positivity" stat={stats.valence} />
+			<h2 className="text-center font-bold uppercase italic tracking-wider text-xl mb-4">
+				Your Spotify stats:
+			</h2>
+			<div className="p-4 border-white rounded-lg border">
+				<StatBar name="Acousticness" stat={stats.acousticness} />
+				<StatBar name="Danceability" stat={stats.danceability} />
+				<StatBar name="Energy" stat={stats.energy} />
+				<StatBar name="Instrumentalness" stat={stats.instrumentalness} />
+				<StatBar name="Positivity" stat={stats.valence} />
+			</div>
 		</div>
 	)
 }
