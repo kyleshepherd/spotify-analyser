60b4f1b40b6b3a50e40094d5ced447091a3914cc
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-09-01 18:44:47 +0100

    Added text to login screen

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,6 +3,9 @@ import React from 'react'
 const LoginButton = ({ authEndpoint, redirectUri, scopes }) => {
 	return (
 		<div className="absolute abs-center">
+			<h1 className="text-center font-semibold mb-4 text-lg">
+				Login to view your Spotify stats
+			</h1>
 			<a
 				className="block text-center bg-green-400 p-4 uppercase rounded-full font-semibold tracking-wider transition-colors duration-200 ease-in-out hover:bg-green-500"
 				href={`${authEndpoint}?client_id=${
